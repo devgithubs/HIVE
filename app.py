7baa6cf8072b1fc5f8bd829dfad7dbcab5481f4e
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-06-08 16:27:22 +0000

    Add test function to run flask app

---
+++ app.py
@@ -0,0 +1,26 @@
+# import os module, env variables, Flask modules needed.
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+#from flask_pymongo import PyMongo
+#from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+# create instance of Flask 
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+# tell the app where and when to run the app. IP & PORT Vars hidden in env.py
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
