cace7df93131c2e68ec66055bd0dcf9eda48f611
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-06-08 16:46:52 +0000

    Add imports for flask-mongodb connection

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-#from flask_pymongo import PyMongo
-#from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
