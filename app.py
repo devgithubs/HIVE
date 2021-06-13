17fe7380ff1c783b7de6b4ef747bd2f153e2ee95
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-06-13 12:39:04 +0000

    Add session logout function, logout flash message and redirect

--- app.py
+++ app.py
@@ -84,8 +84,18 @@ def login():
 def profile(username):
     username = mongo.db.users.find_one(
         {"email": session["user"]})['firstName'].title()
-        
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 
