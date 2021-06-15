f3453de77514c65774d66e1689bbf6e38a132c40
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-06-15 08:38:57 +0000

    Add due_date field to add_task dictionary insert

--- app.py
+++ app.py
@@ -107,6 +107,7 @@ def add_task():
             "task_name": request.form.get("task_name"),
             "task_description": request.form.get("task_description"),
             "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
         mongo.db.tasks.insert_one(task)
