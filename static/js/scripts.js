b33aaa9d874670c7099596f6353da4cfce7e3bed
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-07-26 12:15:04 +0000

    commented out

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $(".datepicker").datepicker({
-    format: "dd-M-yyyy",
-    yearRange: 3,
-    showClearBtn: true,
-    i18n: {
-        done: "Select"
-    }})
-  $(".dropdown-toggle").dropdown();  
-  $(".collapse").collapse('toggle');
+  // $(".datepicker").datepicker({
+  //   format: "dd-M-yyyy",
+  //   yearRange: 3,
+  //   showClearBtn: true,
+  //   i18n: {
+  //       done: "Select"
+  //   }})
+  //$(".dropdown-toggle").dropdown();  
+  //$(".collapse").collapse('toggle');
 });
